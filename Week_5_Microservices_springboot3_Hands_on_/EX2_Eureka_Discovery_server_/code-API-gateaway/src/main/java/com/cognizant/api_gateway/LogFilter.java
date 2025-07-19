836ea531e2f20d9142498acc427493c46483db4f
Jebin Dhas <itzjd2345@gmail.com>
2025-07-19 16:23:41 +0530

    Add files via upload

---
+++ Week_5_Microservices_springboot3_Hands_on_/EX2_Eureka_Discovery_server_/code-API-gateaway/src/main/java/com/cognizant/api_gateway/LogFilter.java
@@ -0,0 +1,25 @@
+package com.cognizant.api_gateway;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class LogFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        System.out.println("🌐 Incoming request URI: " + httpRequest.getRequestURI());
+
+        chain.doFilter(request, response); // Continue the chain
+    }
+}
