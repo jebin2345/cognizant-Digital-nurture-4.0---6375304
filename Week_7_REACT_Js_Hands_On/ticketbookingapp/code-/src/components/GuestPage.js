5d0e701f922cfd2c5ec2419e0162244b4476bdf2
Jebin Dhas <itzjd2345@gmail.com>
2025-08-03 07:29:55 +0530

    Add files via upload

---
+++ Week_7_REACT_Js_Hands_On/ticketbookingapp/code-/src/components/GuestPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function GuestPage() {
+  return (
+    <div>
+      <h2>✈️ Flight Details</h2>
+      <p>Welcome Guest! View available flights.</p>
+      <ul>
+        <li>Flight A: Chennai ➝ Delhi - ₹3200</li>
+        <li>Flight B: Mumbai ➝ Bangalore - ₹2800</li>
+      </ul>
+    </div>
+  );
+}
+
+export default GuestPage;
